4adcf8367c33f569685e25215b35f4c39dafa500
498340147 <114245549+498340147@users.noreply.github.com>
2022-09-24 02:42:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -84,6 +84,3 @@ export const USER_CONFIG = {
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
-        // 相遇
-        {"keyword": "love_day", date: "2022-08-04"},
-        
