0b8f4ad7ce6f9d2ee1dc7e4c159302d5eb57f487
498340147 <114245549+498340147@users.noreply.github.com>
2022-09-24 02:15:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,7 +6,13 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  A
+  APP_ID: "wxdb4f86efce0a7640",
+  
+  // 公众号APP_SECRET
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
+  APP_SECRET: "730d9b6db60e369ee6bcca5cb1f3904c",
+  
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
   IS_SHOW_COLOR: true,
